4dec60209553b3d1d5783200a1a6a62d5c43391a
Matteo Calderaro <matteo.calderaro0@gmail.com>
2022-10-15 12:05:45 +0200

    snake qui marche

--- script.js
+++ script.js
@@ -3,13 +3,14 @@
 (function()
 {
 	// JSON
-	const JSON_FILE = "./save.json";
+	const JSON_FILE = "save.json";
 	
 	// ELT DOM
 	const BUTTONPLAY = document.getElementById("formButtonPlay");
 	const INPUTCOLOR = document.getElementById("colorSnake");
 	const CANVAS = document.getElementById("boardGame");
-	const FORMPARA = document.getElementById("formPara");
+	const GAMEPAGE = document.getElementById("gamePage");
+	const FORMPARA = document.getElementById("paraPage");
 
 	// MAP
 	const CASESIDE = 20; // size of one case in px
@@ -18,59 +19,23 @@
 	const FOOD = 2;
 	const WALL = 3;
 	let world; // map of the world
+	let sizeGrid;
+	let ctx;
 
 	// SNAKE
 	let snakeBody; // tab of snake position
 	let snakeDirection = [1, 0]; // direction vecteur of the snake
+	let foodEat = false;
 
 	// COLOR
 	const RED = "#FF0000";
-	const GREY = "AAAAAA";
-	const WHITE = "FFFFFF";
+	const GREY = "#555555";
+	const WHITE = "#FFFFFF";
+	const LIGHTGREY = "#AAAAAA";
 	
 	// play
-	BUTTONPLAY.addEventListener("click", play2);
-
-	function play2()
-	{
-		// init the game
-		CANVAS.classList.toggle("hidden");
-		FORMPARA.classList.toggle("hidden");
-
-		document.addEventListener("keydown", moveDirection);
-
-		let levelJson;
-
-		fetch(JSON_FILE)
-			.then(function(response)
-			{
-				if (response.ok)
-				{
-					levelJson = response.json();
-				}
-				else
-				{
-					throw ("Err: " + response.status);
-				}
-			})
-			.then(function(levelJson){
-				let levelChose = document.getElementById("level").value;
-				let levels = levelJson.levels;
-
-				for (let level of levels)
-				{
-					if (level.level === levelChose)
-					{
-						levelJson = level;
-					}
-				}
-				console.log(levelJson);
-				initGrid(levelJson);
-			})
-			.catch(function(err){
-				console.log(err);
-			});
-	}
+	let intervalId;
+	BUTTONPLAY.addEventListener("click", play);
 	
 	/**
 	 * function call when we want to play
@@ -78,7 +43,7 @@
 	function play()
 	{
 		// init the game
-		CANVAS.classList.toggle("hidden");
+		GAMEPAGE.classList.toggle("hidden");
 		FORMPARA.classList.toggle("hidden");
 
 		document.addEventListener("keydown", moveDirection);
@@ -87,25 +52,22 @@
 		(async function(){
 			try
 			{
-				console.log(JSON_FILE);
 				let jsonFile = await fetch(JSON_FILE);
-				console.log(JSON_FILE);
 
 				if (jsonFile.ok)
 				{
 					let levelChose = document.getElementById("level").value;
 					let levelsJson = await jsonFile.json();
 					let levels = levelsJson.levels;
-
 					for (let level of levels)
 					{
-						if (level.level === levelChose)
+						if (level.level == levelChose)
 						{
 							levelJson = level;
 						}
 					}
-					console.log(levelJson);
 					initGrid(levelJson);
+					intervalId = setInterval(loopGame, levelJson.delay);
 				}
 				else
 				{
@@ -123,28 +85,39 @@
 	{
 		
 		// empty grid
-		let sizeGrid = levelJson.dimension;
+		world = [];
+		snakeBody = [];
+		sizeGrid = levelJson.dimension;
 		for (let i = 0; i < sizeGrid; i++)
 		{
+			let tabi = [];
 			for (let j = 0; j < sizeGrid; j++)
 			{
-				world[i][j] = EMPTY;
+				tabi.push(EMPTY);
 			}
+			world.push(tabi);
 		}
 
 		// snake position (can not be on a side)
 		let x = Math.floor(Math.random() * (sizeGrid - 2) + 1); 
 		let y = Math.floor(Math.random() * (sizeGrid - 2) + 1);
+		snakeBody.push([x, y]);
 
 		// snake direction (don't go to a side)
-		if (x < sizeGrid / 2)
+		if (x < (sizeGrid / 2))
 		{
 			snakeDirection[0] = 1;
+			snakeBody.unshift([x-1, y]);
+			world[x-1][y] = SNAKE;
 		}
 		else
 		{
 			snakeDirection[0] = -1;
+			snakeBody.unshift([x+1, y]);
+			world[x+1][y] = SNAKE;
 		}
+
+		world[x][y] = SNAKE;
 
 
 		// walls
@@ -158,13 +131,16 @@
 			let caseSideFree = true;
 			for (let caseSide of casesSide)
 			{
-				if (world[caseSide[0]][caseSide[1]] !== EMPTY)
+				if (caseSide[0] >= 0 && caseSide[0] < sizeGrid && caseSide[1] >= 0 && caseSide[1] < sizeGrid)
 				{
-					caseSideFree = false;
+					if (world[caseSide[0]][caseSide[1]] !== EMPTY)
+					{
+						caseSideFree = false;
+					}
 				}
 			}
 
-			while (world[x][y] !== EMPTY && caseSideFree)
+			while (world[x][y] !== EMPTY && !caseSideFree)
 			{
 				x = Math.floor(Math.random() * sizeGrid);
 				y = Math.floor(Math.random() * sizeGrid);
@@ -177,11 +153,63 @@
 		generateApple(levelJson.dimension);
 
 		// draw the board
-		drawCanva(levelJson.dimension);
-
-		setInterval(levelJson.delay, loopGame);
-
+		CANVAS.setAttribute("width", sizeGrid * CASESIDE);
+		CANVAS.setAttribute("height", sizeGrid * CASESIDE);
+		ctx = CANVAS.getContext('2d');
+		drawCanva();
 		
+	}
+
+	function loopGame()
+	{
+		let snakeHead = [snakeBody[snakeBody.length - 1][0], snakeBody[snakeBody.length - 1][1]];
+		if (snakeDirection[0] === 1)
+		{
+			snakeBody.push([snakeHead[0] + 1, snakeHead[1]]);
+		}
+		else if (snakeDirection[0] === -1)
+		{
+			snakeBody.push([snakeHead[0] - 1, snakeHead[1]]);
+		}
+		else if (snakeDirection[1] === 1)
+		{
+			snakeBody.push([snakeHead[0], snakeHead[1] + 1]);
+		}
+		else
+		{
+			snakeBody.push([snakeHead[0], snakeHead[1] - 1]);
+		}
+
+		let snakeTail = snakeBody[0];
+
+		snakeHead = [snakeBody[snakeBody.length - 1][0], snakeBody[snakeBody.length - 1][1]];
+
+		if (snakeHead[0] > sizeGrid - 1 || snakeHead[1] > sizeGrid - 1 || snakeHead[0] < 0 || snakeHead[1] < 0)
+		{
+			endGame();
+		}
+		if (world[snakeHead[0]][snakeHead[1]] === SNAKE || world[snakeHead[0]][snakeHead[1]] === WALL)
+		{
+			endGame();	
+		}
+		if (world[snakeHead[0]][snakeHead[1]] === FOOD)
+		{
+			generateApple();
+		}
+		else
+		{
+			snakeBody.shift();
+		}
+
+		putSnakeOnWorld(snakeTail);
+		drawCanva();
+	}
+
+	function endGame()
+	{
+		clearInterval(intervalId);
+		GAMEPAGE.classList.toggle("hidden");
+		FORMPARA.classList.toggle("hidden");
 	}
 
 	function moveDirection(evt)
@@ -212,19 +240,31 @@
 		{
 			if (snakeDirection[0] !== 1)
 			{
-				snakeDirection = [0, -1];
+				snakeDirection = [-1, 0];
 			}
 		}
 	}
-
-	function drawCanva(sizeGrid)
+	
+	function generateApple()
 	{
+		let x = Math.floor(Math.random() * sizeGrid);
+		let y = Math.floor(Math.random() * sizeGrid);
+		while (world[x][y] !== EMPTY)
+		{
+			x = Math.floor(Math.random() * sizeGrid);
+			y = Math.floor(Math.random() * sizeGrid);
+		}
+
+		world[x][y] = FOOD;
+	}
+
+	function drawCanva()
+	{
+		ctx.clearRect(0, 0, sizeGrid * CASESIDE, sizeGrid * CASESIDE);
 		let colorSnake = INPUTCOLOR.value;
 		let colorFood = RED;
 		let colorWall = GREY;
-		let colorEmpty = WHITE;
-
-		let ctx = CANVAS.getContext('2d');
+		let colorEmpty = LIGHTGREY;
 
 		for (let i = 0; i < sizeGrid; i++)
 		{
@@ -239,6 +279,12 @@
 				{
 					ctx.fillStyle = colorSnake;
 					ctx.fillRect(i * CASESIDE, j * CASESIDE, CASESIDE, CASESIDE);
+					if (i === snakeBody[snakeBody.length - 1][0] && j === snakeBody[snakeBody.length - 1][1])
+					{
+						ctx.fillStyle = RED;
+						ctx.fillRect(i * CASESIDE + CASESIDE / 3, j * CASESIDE + CASESIDE / 3, CASESIDE / 3, CASESIDE / 3);
+					}
+					
 				}
 				if (world[i][j] === FOOD)
 				{
@@ -255,23 +301,15 @@
 
 	}
 
-	function generateApple(sizeGrid)
+	function putSnakeOnWorld(snakeTail)
 	{
-		let x = Math.floor(Math.random() * sizeGrid);
-		let y = Math.floor(Math.random() * sizeGrid);
-		while (world[x][y] !== EMPTY)
+		for (let snakePart of snakeBody)
 		{
-			x = Math.floor(Math.random() * sizeGrid);
-			y = Math.floor(Math.random() * sizeGrid);
+			let x = snakePart[0];
+			let y = snakePart[1];
+			world[x][y] = SNAKE;
 		}
-
-		world[x][y] = FOOD;
-	}
-
-	function loopGame()
-	{
-
-		CANVAS.classList.toggle("hidden");
+		world[snakeTail[0]][snakeTail[1]] = EMPTY;
 	}
 
 })();
